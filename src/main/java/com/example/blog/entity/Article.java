849c07fdbf937f063ffa89dfbadbf29a5dd61502
Moonlight0124 <1474876326@qq.com>
2024-06-02 10:56:23 +0800

    entity更新

--- src/main/java/com/example/blog/entity/Article.java
+++ src/main/java/com/example/blog/entity/Article.java
@@ -22,5 +22,68 @@ public class Article {
     @JoinColumn(name = "category_id")
     private Category category;
 
-    // Getters and setters
+    public Article() {
+    }
+
+    public Article(Long id, String title, String content, User user, Category category) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.category = category;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+    public Category getCategory() {
+        return category;
+    }
+
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String toString() {
+        return "Article{id = " + id + ", title = " + title + ", content = " + content + ", user = " + user + ", category = " + category + "}";
+    }
+
 }
--- src/main/java/com/example/blog/entity/Category.java
+++ src/main/java/com/example/blog/entity/Category.java
@@ -16,5 +16,44 @@ public class Category {
     @JoinColumn(name = "parent_id")
     private Category parent;
 
-    // Getters and setters
+    public Category() {
+    }
+
+    public Category(Long id, String name, Category parent) {
+        this.id = id;
+        this.name = name;
+        this.parent = parent;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Category getParent() {
+        return parent;
+    }
+
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+
+    public String toString() {
+        return "Category{id = " + id + ", name = " + name + ", parent = " + parent + "}";
+    }
+
 }
