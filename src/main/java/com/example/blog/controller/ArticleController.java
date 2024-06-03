ad420fc203d26f46ec19855ee78e3d05faf7622a
kevin <kevin0x00@163.com>
2024-06-03 14:49:33 +0800

    试验Home:登陆成功后，跳转Home页面，显示Hello, <用户名>

--- src/main/java/com/example/blog/controller/ArticleController.java
+++ src/main/java/com/example/blog/controller/ArticleController.java
@@ -1,13 +1,17 @@
 package com.example.blog.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ArticleController {
 
     @GetMapping("/home")
-    public String showHomePage() {
+    public String showHomePage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        model.addAttribute("name", userDetails.getUsername());
         return "home"; // 返回登录页面的视图名称
     }
 
