27671aac72a7c68cba61aa83031b285819add761
Moonlight0124 <1474876326@qq.com>
2024-06-06 15:31:09 +0800

    更新：文章分类页面后端代码

--- src/main/java/com/example/blog/controller/CategoryController.java
+++ src/main/java/com/example/blog/controller/CategoryController.java
@@ -2,8 +2,10 @@ package com.example.blog.controller;
 
 
 import com.example.blog.data.CategoryTreeNode;
+import com.example.blog.entity.Article;
 import com.example.blog.entity.Category;
 import com.example.blog.entity.User;
+import com.example.blog.service.ArticleService;
 import com.example.blog.service.CategoryService;
 import com.example.blog.service.UserService;
 
@@ -11,6 +13,9 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -27,6 +32,8 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private ArticleService articleService;
     @ModelAttribute
     public void checkUser(@PathVariable("username") String username) throws ModelAndViewDefiningException {
         // 获取当前登录用户的用户名
@@ -42,11 +49,21 @@ public class CategoryController {
     }
 
     @GetMapping("/{username}/categories")
-    public String showCategoryPage(@PathVariable("username") String username, Model model) {
+    public String showCategoryPage(@PathVariable("username") String username,
+                                   @RequestParam(name = "page", defaultValue = "0") int page,
+                                   @RequestParam(value = "categoryId", required = false) Long categoryId, Model model) {
         User user = userService.findUserByUsername(username);
         List<Category> categories = categoryService.getAllCategories();
         CategoryTreeNode root = null;
         HashMap<Long, CategoryTreeNode> nodes = new HashMap<>();
+        Pageable pageable = PageRequest.of(page, 10); // 每页显示10篇文章
+        Page<Article> articles = null;
+        if (categoryId != null) {
+            Category categoryParam = categoryService.getCategoryById(categoryId);
+            articles = articleService.getArticlesByCategoryPaged(user.getId(), categoryParam, pageable);
+        } else {
+            articles = articleService.getArticlesByUserIdPaged(user.getId(), pageable);
+        }
         for (Category category : categories) {
             CategoryTreeNode currentNode = new CategoryTreeNode(category.getId(), category.getName());
             nodes.put(category.getId(), currentNode);
@@ -60,8 +77,11 @@ public class CategoryController {
             }
         }
 
+        model.addAttribute("articles", articles.getContent());
         model.addAttribute("rootCategory", root);
         model.addAttribute("user", user);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", articles.getTotalPages());
         return "categories";
     }
 }
