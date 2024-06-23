24a2eeec463cbed604dc60362209282e34d28dc6
kevin <kevin0x00@163.com>
2024-06-23 18:17:38 +0800

    修复:SearchController检查用户

--- src/main/java/com/example/blog/controller/SearchController.java
+++ src/main/java/com/example/blog/controller/SearchController.java
@@ -1,16 +1,23 @@
 package com.example.blog.controller;
 
-import com.example.blog.entity.User;
-import com.example.blog.service.SearchService;
-import com.example.blog.service.UserService;
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndViewDefiningException;
+import org.springframework.web.servlet.view.RedirectView;
 
-import java.time.Instant;
+import com.example.blog.entity.User;
+import com.example.blog.service.SearchService;
+import com.example.blog.service.UserService;
 
 @Controller
 public class SearchController {
@@ -18,6 +25,20 @@ public class SearchController {
   private UserService userService;
   @Autowired
   private SearchService searchService;
+
+  @ModelAttribute
+  public void checkUser(@PathVariable("username") String username) throws ModelAndViewDefiningException {
+    // 获取当前登录用户的用户名
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    String loggedInUsername = authentication.getName();
+
+    // 如果当前登录用户与要访问的用户不匹配，则进行相应处理
+    if (!loggedInUsername.equals(username)) {
+      String redirectUrl = "/" + loggedInUsername + "/home";
+      RedirectView redirectView = new RedirectView(redirectUrl);
+      throw new ModelAndViewDefiningException(new ModelAndView(redirectView));
+    }
+  }
 
   @GetMapping("/{username}/search-articles")
   public String searchArticles(@PathVariable("username") String username,
